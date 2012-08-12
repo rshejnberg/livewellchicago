8aaa5c46c7901aec017307d239954919d291e901
Raphael Shejnberg <raphaelshejnberg@Raphaels-MacBook-Pro.local>
2012-08-12 13:52:01 -0500

    commit for deploy to heroku

--- db/migrate/20120531233930_remove_laundy_column_from_apartments.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLaundyColumnFromApartments < ActiveRecord::Migration
-  def up
-    remove_column :apartments, :laundy
-      end
-
-  def down
-    add_column :apartments, :laundy, :string
-  end
-end
