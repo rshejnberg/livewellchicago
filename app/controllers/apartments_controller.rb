9865da7acd64c537ed7e02bcccaa53c11da5dffd
Raphael Shejnberg <raphaelshejnberg@Raphaels-MacBook-Pro.local>
2012-08-14 15:22:47 -0500

    commit for deploy to heroku

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -14,7 +14,7 @@ class ApartmentsController < ApplicationController
     
   def aptMenu
       @apartments = Apartment.all
-      @studio = Apartment.where(:bed => '0').all
+      @studio = Apartment.where(:Bed => '0').all
       @oneBdr = Apartment.find(:all, :conditions => ["bed = 1"])
       @twoBdr = Apartment.find(:all, :conditions => ["bed = 2"])
       @threeBdr = Apartment.find(:all, :conditions => ["bed = 3"])
