52916105c7ef1614b577b91a8550bdf7f52ef6a8
Raphael Shejnberg <raphaelshejnberg@Raphaels-MacBook-Pro.local>
2012-08-17 13:11:40 -0500

    commit for deploy to heroku

--- app/assets/javascripts/livewell.js
+++ app/assets/javascripts/livewell.js
@@ -220,5 +220,41 @@ $(document).ready(function()
                                       });
                   });
 
+//Url Parser (From Reflection at webmasterworld.com /*
+var category = QueryString("cat");
+if(category == "forms"){showForms();}
+function QueryString(key) 
+{ 
+var value = null; 
+for (var i=0;i<QueryString.keys.length;i++) 
+{ 
+if (QueryString.keys[i]==key) 
+{ 
+value = QueryString.values[i]; 
+break; 
+} 
+} 
+return value; 
+} 
+QueryString.keys = new Array(); 
+QueryString.values = new Array();
 
+function QueryString_Parse() 
+{ 
+var query = window.location.search.substring(1); 
+var pairs = query.split("&");
+
+for (var i=0;i<pairs.length;i++) 
+{ 
+var pos = pairs[i].indexOf('='); 
+if (pos >= 0) 
+{ 
+var argname = pairs[i].substring(0,pos); 
+var value = pairs[i].substring(pos+1); 
+QueryString.keys[QueryString.keys.length] = argname; 
+QueryString.values[QueryString.values.length] = value; 
+} 
+} 
+} 
+QueryString_Parse()
 
