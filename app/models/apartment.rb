8ae97fdc537ea65e5fed4b7abe220df4db19f330
Raphael Shejnberg <raphaelshejnberg@Raphaels-MacBook-Pro.local>
2012-08-22 00:09:52 -0500

    commit for deploy to heroku

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,11 +1,11 @@
 class Apartment < ActiveRecord::Base
-    attr_accessible :AC, :Bath, :Bed, :Date_Avail, :Laundry, :Owner, :Parking, :Pets, :Rent, :Security_Deposit, :Type_Heat, :location, :availability, :agent, :description, :photo, :photo2, :photo3, :photo4
+    attr_accessible :AC, :Bath, :Bed, :Date_Avail, :Laundy, :Owner, :Parking, :Pets, :Rent, :Security_Deposit, :Type_Heat, :location, :availability, :agent, :description, :photo, :photo2, :photo3, :photo4
     
     has_many :apartment_image, :dependent => :destroy
     
     accepts_nested_attributes_for :apartment_image, :reject_if => 	 lambda { |t| t['apartment_image'].nil? }
-    has_attached_file :photo, :styles => {:small => "150x150>", :large => "800x720>"}
-    has_attached_file :photo2, :styles => {:small => "150x150>", :large => "800x720>"}
+    has_attached_file :photo, :styles => {:small => "150x150>", :large => "800x720>", :convert_options => { :all => 	'-auto-orient' }}
+    has_attached_file :photo2, :styles => {:small => "150x150>", :large => "800x720>" , :convert_options => { :all => 	'-auto-orient' }}
     has_attached_file :photo3, :styles => {:small => "150x150>", :large => "800x720>"}
     has_attached_file :photo4, :styles => {:small => "150x150>", :large => "800x720>"}
     AGENTS = ['Jason Gold', 'Serafin Herrera', 'Alexia Kouklowsky']
