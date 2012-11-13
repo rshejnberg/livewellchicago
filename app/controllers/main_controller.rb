371377d17166d1180e4a1f42fa0284468b863f8f
rshejnberg <raphael.shejnberg@gmail.com>
2012-11-13 00:46:48 -0600

    ds

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,11 +3,11 @@ class MainController < ApplicationController
     skip_before_filter :authorize, :only => :home
     def home
       @apartments = Apartment.all
-      @studio = Apartment.where("bed = '0'").all
-      @oneBdr = Apartment.where("bed = '1'").all
-      @twoBdr = Apartment.where("bed = '2'").all
-      @threeBdr = Apartment.where("bed = '3'").all
-      @fourPlusBdr = Apartment.where("bed = '4'").all   
+      @studio = Apartment.find(:all, :conditions => ["bed = 0"])
+      @oneBdr = Apartment.find(:all, :conditions => ["bed = 0"])
+      @twoBdr = Apartment.find(:all, :conditions => ["bed = 0"])
+      @threeBdr = Apartment.find(:all, :conditions => ["bed = 0"])
+      @fourPlusBdr = Apartment.find(:all, :conditions => ["bed = 0"])   
   end
 
   def admin
